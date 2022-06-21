2fe7997ed671df52bde3006e0ab5aaa620db5407
Jon Lervold <jonlervold@gmail.com>
2022-06-20 19:49:28 -0600

    Initial commit, pull code from legacy repo

---
+++ apps/app/src/app/api/getAi.tsx
@@ -0,0 +1,39 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const getAi = async (input: string, key: string) => {
+  const configuration = new Configuration({
+    apiKey: key,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const response = await openai.createCompletion({
+    model: 'text-davinci-002',
+    prompt: input,
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+  return response;
+};
+
+// const configuration = new Configuration({
+//   apiKey: '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',
+// });
+// const openai = new OpenAIApi(configuration);
+
+// const getAi = async (input: string) => {
+//   const response = await openai.createCompletion({
+//     model: 'text-davinci-002',
+//     prompt: input,
+//     temperature: 0.7,
+//     max_tokens: 256,
+//     top_p: 1,
+//     frequency_penalty: 0,
+//     presence_penalty: 0,
+//   });
+//   return response;
+// };
+
+export default getAi;
--- apps/app/src/app/app.tsx
+++ apps/app/src/app/app.tsx
@@ -1,15 +1,113 @@
 import styled from '@emotion/styled';
-import NxWelcome from './nx-welcome';
+import { useState } from 'react';
+import getAi from './api/getAi';
 
-const StyledApp = styled.div`
-  // Your style here
+const Container = styled.div`
+  text-align: center;
+  button {
+    width: 50%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  textarea {
+    margin: 1rem;
+    margin-left: auto;
+    margin-right: auto;
+    width: 90%;
+    height: 3in;
+  }
+`;
+
+const Card = styled.div`
+  border: 1px solid black;
+  width: 90%;
+  margin: 1rem;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: left;
+  .header {
+    font-weight: bold;
+  }
+  .timestamp {
+    margin: 1rem;
+  }
+  .query {
+    margin: 1rem;
+  }
+  .response {
+    margin: 1rem;
+    white-space: pre-line;
+  }
 `;
 
 export function App() {
+  const [input, setInput] = useState('');
+  const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(e.currentTarget.value);
+  };
+  const [key, setKey] = useState('');
+  const handleKey = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setKey(e.currentTarget.value);
+  };
+  const [output, setOutput] = useState<
+    {
+      timestamp: Date;
+      query: string;
+      response: string | undefined;
+    }[]
+  >([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const dateTime = new Date();
+  const handleSubmit = async () => {
+    setIsLoading(true);
+    const response = await getAi(input, key);
+    if (response.data.choices !== undefined) {
+      const thisQuery = {
+        timestamp: dateTime,
+        query: input,
+        response: response.data.choices[0].text,
+      };
+      setOutput([thisQuery, ...output]);
+    }
+    setIsLoading(false);
+  };
+
   return (
-    <StyledApp>
-      <NxWelcome title="app" />
-    </StyledApp>
+    <Container>
+      <h1>TALK2AI</h1>
+      <div>
+        API KEY
+        <input type="password" value={key} onChange={(e) => handleKey(e)} />
+      </div>
+      <div>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</div>
+      <textarea value={input} onChange={(e) => handleInput(e)}></textarea>{' '}
+      <button disabled={isLoading} onClick={handleSubmit}>
+        {isLoading ? 'Loading...' : 'Submit'}
+      </button>
+      {output.map((instance, index) => (
+        <Card>
+          <div className="header">{index + 1}</div>
+          <div className="timestamp">{instance.timestamp.toLocaleString()}</div>
+          <div className="query">
+            {' '}
+            <div className="header">Query</div>
+            {instance.query}
+          </div>
+          <div className="response">
+            {' '}
+            <div className="header">Response</div>
+            {instance.response}
+          </div>
+        </Card>
+      ))}
+      {/* <div>
+        response.data.choices[0].finish_reason - model: "text-davinci-002",
+        prompt: "CHECK", temperature: 0.7, max_tokens: 256, top_p: 1,
+        frequency_penalty: 0, presence_penalty: 0, remove axios - remove express
+        - input bar for api key
+      </div> */}
+    </Container>
   );
 }
 
